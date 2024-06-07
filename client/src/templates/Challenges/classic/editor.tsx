accdf3e6d2068cc802b8d90fa615943b8af5f194
sembauke <semboot699@gmail.com>
2024-06-07 08:35:55 +0200

    Revert Collapse code

--- client/src/templates/Challenges/classic/editor.tsx
+++ client/src/templates/Challenges/classic/editor.tsx
@@ -60,7 +60,6 @@ import {
 import GreenPass from '../../../assets/icons/green-pass';
 import {
   enhancePrismAccessibility,
-  makePrismCollapsible,
   setScrollbarArrowStyles
 } from '../utils/index';
 import { initializeMathJax } from '../../../utils/math-jax';
@@ -808,7 +807,6 @@ const Editor = (props: EditorProps): JSX.Element => {
     descContainer.appendChild(desc);
     desc.innerHTML = description;
     Prism.hooks.add('complete', enhancePrismAccessibility);
-    Prism.hooks.add('complete', makePrismCollapsible);
     Prism.highlightAllUnder(desc);
 
     domNode.style.userSelect = 'text';
